6f9632e0f3ad61e6968d12fa8e126a4e694f717e
mv735065 <245119735065@mvsrec.edu.in>
2025-04-23 17:18:21 +0530

    Completed ipl in pandas

--- src/data.py
+++ src/data.py
@@ -1,33 +1,15 @@
 
-
+import pandas as pd
 import csv
-
-dataMatches = []
-
 dataDeliveries=[]
 
-# Open the CSV file
-with open('public/matches.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)  
-    for row in reader:
-        dataMatches.append(row)
+# dfMatches=pd.read_csv('public/matches.csv')
+# dfDeleiveries=pd.read_csv('public/deliveries.csv')
 
 
-# Open the CSV file
-with open('public/deliveries.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)  
-    for row in reader:
-        dataDeliveries.append(row)
+with open('public/deliveries.csv','r') as file:
+    reader=csv.DictReader(file)
+    dataDeliveries=[row for row in reader]
 
 
 
-# with open('data.csv',mode='r',encoding='utf-8') as file:
-#     reader=csv.DictReader(file)
-#     for row in reader:
-#         dataMatches.append(row)
-
-
-# import json
-
-# with open('public/data.json', mode='r', encoding='utf-8') as file:
-#     data = json.load(file)  # Loads the entire JSON into a Python dict (or list, depending on the structure)
